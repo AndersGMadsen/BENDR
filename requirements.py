f2f68a56534935058963c8d970d37d98fedfbe26
Anders <andersgj.madsen@gmail.com>
2023-12-21 11:32:50 +0100

    Requirements

---
+++ requirements.py
@@ -0,0 +1,26 @@
+# Python 3.8.18
+
+tqdm
+parse
+pyyaml
+pyyaml-include
+objgraph
+openpyxl
+XlsxWriter
+
+numpy
+pandas
+matplotlib
+seaborn
+
+scikit-learn
+
+torch==1.7.1+cu110
+torchvision==0.8.2+cu110
+torchaudio==0.7.2
+-f https://download.pytorch.org/whl/torch_stable.html
+
+mne
+
+# DN3 version 0.2
+git+https://github.com/AndersGMadsen/dn3.git#egg=dn3
